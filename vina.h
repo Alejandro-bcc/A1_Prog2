5112a325027e80f082127167ae0d61654763adb5
Aleajandro <alejandro.nava@ufpr.br>
2025-04-23 18:47:57 -0300

    Files

--- vina.h
+++ vina.h
@@ -27,7 +27,7 @@ unsigned int arquivo_para_buffer(FILE *arq, unsigned char **buffer);
 
 int buffer_para_arquivo(unsigned char *buffer, unsigned int tam_arq, FILE *arq);
 
-struct membro * cria_membro();
+struct membro * cria_membro(FILE *arq);
 
 struct diretorio * cria_diretorio();
 
